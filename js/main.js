1e21c1fb4acbcfc69889463c6051dee077e8e5d8
Tagir <gtagir11@yandex.ru>
2019-08-23 00:24:52 +0500

    footer + i forget what else

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// document.addEventListener('DOMContentLoaded' () => {
+    
+// });
+// window.addEventListener('load', function () {
+//     let subtitle = document.querySelector('.subtitle');
+//     setTimeout(() => {
+//         console.log(subtitle);
+//         subtitle.classList.add('animated');
+//     }, 2000);
+// });
