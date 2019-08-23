7752ac8782b2fdc0fe62cd600d91cfaebacc2e39
Tagir <gtagir11@yandex.ru>
2019-08-23 16:12:03 +0500

    return to top button

--- js/scroll.js
+++ js/scroll.js
@@ -2,6 +2,21 @@ $(document).on('click', 'a[href^="#"]', function (event) {
   event.preventDefault();
 
   $('html, body').animate({
-      scrollTop: $($.attr(this, 'href')).offset().top
+    scrollTop: $($.attr(this, 'href')).offset().top
   }, 1000);
+});
+
+// ===== Scroll to Top ==== 
+$(window).scroll(function () {
+  if ($(this).scrollTop() >= document.documentElement.clientHeight) {        // If page is scrolled more than 50px
+    $('#return-to-top').fadeIn(500);    // Fade in the arrow
+  } else {
+    $('#return-to-top').fadeOut(500);   // Else fade out the arrow
+  }
+});
+$('#return-to-top').click(function (event) {      // When arrow is clicked
+  event.preventDefault();
+  $('body,html').animate({
+    scrollTop: 0                       // Scroll to top of body
+  }, 500);
 });
