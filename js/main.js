7f0f6ec5c18c7c1bdeb6ad6c68a311a5ed195be9
Tagir <gtagir11@yandex.ru>
2019-08-24 12:18:49 +0500

    birthday script

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
-// document.addEventListener('DOMContentLoaded' () => {
-    
-// });
-// window.addEventListener('load', function () {
-//     let subtitle = document.querySelector('.subtitle');
-//     setTimeout(() => {
-//         console.log(subtitle);
-//         subtitle.classList.add('animated');
-//     }, 2000);
-// });
+document.addEventListener('DOMContentLoaded', () => {
+    console.image('https://i.imgur.com/Y8fM5v3.png', size=0.2);
+    console.log('Не бейте за говно вёрстку и JQ плииз');
+    let age = document.querySelector(".age"),
+        birthday = new Date(2005,2,11);
+    age.textContent = new Date(Date.now() - birthday).getFullYear() - 1970
+});
