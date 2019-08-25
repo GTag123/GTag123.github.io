2b586e2b4b3958bb91a84429dd534447dc3fdd56
Tagir <gtagir11@yandex.ru>
2019-08-25 23:27:03 +0500

    changed console.log message

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.image('https://i.imgur.com/Y8fM5v3.png', size=0.2);
-    console.log('Не бейте за говно вёрстку и JQ плииз');
+    console.image('https://i.imgur.com/RGcoujP.png', size=0.1);
+    console.log('Люди добрые простите за jquery пж! Скролл на нативном js получился корявый');
     let age = document.querySelector(".age"),
         birthday = new Date(2005,2,11);
     age.textContent = new Date(Date.now() - birthday).getFullYear() - 1970
