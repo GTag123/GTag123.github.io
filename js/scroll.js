56a42811478d4e1043976a3fe0429bcfe9ad83f6
Tagir <gtagir11@yandex.ru>
2019-08-25 22:32:35 +0500

    Try fix retur-to-top:hover in mobile browser

--- js/scroll.js
+++ js/scroll.js
@@ -1,28 +1,35 @@
-$(document).on('click', 'a[href^="#"]', function (event) {
-  event.preventDefault();
+$(document).ready(function () {
+  $(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
 
-  $('html, body').animate({
-    scrollTop: $($.attr(this, 'href')).offset().top
-  }, 1000);
-});
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+  });
 
-// ===== Scroll to Top ==== 
-$(window).scroll(function () {
-  if ($(this).scrollTop() >= document.documentElement.clientHeight) {        // If page is scrolled more than 50px
-    $('#return-to-top').fadeIn({
+  // ===== Scroll to Top ==== 
+  $(window).scroll(function () {
+    if ($(this).scrollTop() >= document.documentElement.clientHeight) {        // If page is scrolled more than 50px
+      $('#return-to-top').fadeIn({
         duration: "fast",
         start: function () {
-            $(this).css({
-                display: "flex"
-            });
-        }});    // Fade in the arrow
-  } else {
-    $('#return-to-top').fadeOut(500);   // Else fade out the arrow
-  }
-});
-$('#return-to-top').click(function (event) {      // When arrow is clicked
-  event.preventDefault();
-  $('body,html').animate({
-    scrollTop: 0                       // Scroll to top of body
-  }, 500);
+          $(this).css({
+            display: "flex"
+          });
+        }
+      });    // Fade in the arrow
+    } else {
+      $('#return-to-top').fadeOut(500);   // Else fade out the arrow
+    }
+  });
+  $('#return-to-top').click(function (event) {      // When arrow is clicked
+    event.preventDefault();
+    $('body,html').animate({
+      scrollTop: 0                       // Scroll to top of body
+    }, 500);
+  });
+  $('#return-to-top').on('touchstart touchend', function (e) {
+    e.preventDefault();
+    $(this).toggleClass('to-top-hover');
+  });
 });
