52d7306bbc8ecc7364a262b25563a8fe7c9d357b
Tagir <gtagir11@yandex.ru>
2020-08-14 12:37:55 +0500

    slight age changes))

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.image('https://i.imgur.com/RGcoujP.png', size=0.1);
-    console.log('Люди добрые простите за jquery пж! Скролл на нативном js получился корявый');
+    console.log('jquery хрень кста как оно тут появилось');
     let age = document.querySelector(".age"),
         birthday = new Date(2005,2,11);
     age.textContent = new Date(Date.now() - birthday).getFullYear() - 1970;
